320381e605bfb547326865430ca2dc80b21db4f9
kcloze <lixiaopei@yaochufa.com>
2016-06-17 18:32:24 +0800

    增加测试表数据

---
+++ database.sql
@@ -0,0 +1,34 @@
+/*
+SQLyog Ultimate v9.62 
+MySQL - 5.6.22-log : Database - test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `counters` */
+
+DROP TABLE IF EXISTS `counters`;
+
+CREATE TABLE `counters` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `value` int(11) DEFAULT NULL,
+  `name` varchar(10) NOT NULL DEFAULT '' COMMENT 'X',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `counters` */
+
+insert  into `counters`(`id`,`value`,`name`) values (1,100,'kcloze'),(2,33,'tom'),(3,233,'oop'),(4,89,'llo');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
